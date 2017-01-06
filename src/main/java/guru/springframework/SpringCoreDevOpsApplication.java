ddcd96629246df5323a80bcb1e73fedae6953e11
Administrator <user@example.com>
2017-01-06 01:48:59 +0100

    Testing CI build webhook

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreDevOpsApplication {
 
 	//test
+	//another test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 	}
